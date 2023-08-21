d1a8fe12e662e63fbf2c37435fa6d6d4d5988fbb
馬思遥 <2332874834@qq.com>
2023-08-21 14:53:33 +0900

    社員情報登録・更新 製造

--- SSM_Project/src/main/java/com/ssm/framework/team1/controller/FormController.java
+++ SSM_Project/src/main/java/com/ssm/framework/team1/controller/FormController.java
@@ -1,8 +1,10 @@
 package com.ssm.framework.team1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.ssm.framework.team1.entity.Form;
+import com.ssm.framework.team1.repository.FormRepository;
 
 
 
@@ -10,20 +12,42 @@ import com.ssm.framework.team1.entity.Form;
 @RequestMapping("/employee")
 public class FormController {
 	
-	@GetMapping("/create")
-	public String createForm(@ModelAttribute("form") Form form) {
-		return "form"; 
+	@Autowired
+	private FormRepository formRepository;
+	 
+	@GetMapping("/")
+	public String showForm() {
+		return "form";
 	}
 	 
-	@PostMapping("/create")
-    public String saveEmployee(@ModelAttribute("form") Form form) {
-		if (form.getEmployeeId() == null || form.getEmployeeId().isEmpty() ||
-				form.getName() == null || form.getName().isEmpty() ||
-				form.getDepartment() == null || form.getDepartment().isEmpty() ||
-				form.getAddress() == null || form.getAddress().isEmpty()) {
-			 return "result-failure";
-		}
+	@PostMapping("/submit")
+    public String createForm(
+    		@RequestParam("id") String id, 
+    		@RequestParam("name") String name, 
+    		@RequestParam("department") String department,
+    		@RequestParam("gender") String gender,
+    		@RequestParam("telephone") String telephone,
+    		@RequestParam("date") String date,
+    		@RequestParam("age") String age,
+    		@RequestParam("address") String address) {
+		Form form = new Form();
+		form.setEMPLOYEE_ID(id);
+		form.setEMPLOYEE_NAME(name);
+		form.setDEPT_ID(department);
+		form.setGENDER_ID(gender);
+		form.setTELEPHONE_NUMBER(telephone);
+		form.setENTRY_DATE(date);
+		form.setAGE(age);
+		form.setMAIL_ADDRESS(address);
+		formRepository.save(form);
 
-        return "result-success"; 
+//		if (form.getEmployeeId() == null || form.getEmployeeId().isEmpty() ||
+//				form.getName() == null || form.getName().isEmpty() ||
+//				form.getDepartment() == null || form.getDepartment().isEmpty() ||
+//				form.getAddress() == null || form.getAddress().isEmpty()) {
+//			 return "result-failure";
+//		}
+
+		return "redirect:/employeelist";
     }
 }
