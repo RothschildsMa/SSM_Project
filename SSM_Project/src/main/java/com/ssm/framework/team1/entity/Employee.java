795d232bd9bf924969aea3781efc4eead4c405ee
Sandi <82432916+SuMyat-Sandi@users.noreply.github.com>
2023-08-28 18:03:22 +0900

    修正

--- SSM_Project/src/main/java/com/ssm/framework/team1/entity/Employee.java
+++ SSM_Project/src/main/java/com/ssm/framework/team1/entity/Employee.java
@@ -1,14 +1,15 @@
 package com.ssm.framework.team1.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import lombok.Data;
 
 @Data
 public class Employee implements Serializable {
 	private String employeeId;
-	private String entryDateFrom;
-	private String entryDateTo;
+	private String dateFrom;
+	private String dateTo;
 	private String entryDate;
 	private String employeeName;
 	private String genderId;
